70766cd79c8a9b27745611e621d1050a69f516e9
Dan Sullivan <dansullivan@gmail.com>
2022-05-11 17:27:26 -0400

    feat: initialize firebase

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const admin = require('firebase-admin')
+const AWS = require("aws-sdk");
+const ssm = new AWS.SSM();
+
 exports.handler = async (event) => {
-    console.info('Here')
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    const data= await ssm.getParameters({
+        Names: ['staging-firebase-project-id', 'staging-firebase-private-key', 'staging-firebase-client-email' ]
+    }).promise();
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: data.Parameters[0].Value,
+        privateKey: privateKey,
+        clientEmail: data.Parameters[2].Value,
+      })
+    })
 };

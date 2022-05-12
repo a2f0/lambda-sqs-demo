2f82e849a0b9d6249baed7974eb809d5e17a19b3
Dan Sullivan <dansullivan@gmail.com>
2022-05-12 13:26:28 -0400

    got delivery working

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const AWS = require("aws-sdk");
 const ssm = new AWS.SSM();
 const { v4: uuidv4 } = require('uuid')
 
-
-
 exports.handler = async (event) => {
-    const data= await ssm.getParameters({
-      Names: ['staging-firebase-project-id', 'staging-firebase-private-key', 'staging-firebase-client-email' ]
+    const paramaters = await ssm.getParameters({
+      Names: ['staging-firebase-config']
     }).promise();
-
+    const jsonFirebaseConfig = JSON.parse(paramaters.Parameters[0].Value);
     if(firebase.apps.length == 0) { 
       firebase.initializeApp({
         credential: firebase.credential.cert({
-            projectId: data.Parameters[0].Value,
-            privateKey: data.Parameters[1].Value,
-            clientEmail: data.Parameters[2].Value,
+            projectId: jsonFirebaseConfig.project_id,
+            privateKey: jsonFirebaseConfig.private_key, 
+            clientEmail: jsonFirebaseConfig.client_email 
           })
         })
     }
